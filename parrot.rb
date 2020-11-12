d9be30513486f7bba23c6b37ae67c725bd35eef5
adelbertclerge <adelbertclerge@gmail.com>
2020-11-12 10:15:51 -0500

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(wildbird = "Squawk!")
+  puts "#{wildbird}"
+  "#{wildbird}"
+end
+
+
+def door(wood="creak!")
+  puts "#{wood}"
+  "#{wood}"
+end
